da110aeff0d6edcc0ed3b5fbd9a79311340b1ec0
IbrahimNashaat <ibrahimnashaat@matnsolutions.com>
2025-02-11 14:28:54 +0200

    update chart

--- lib/k_chart_widget.dart
+++ lib/k_chart_widget.dart
@@ -366,7 +366,7 @@ class KChartWidgetState extends State<KChartWidget>
                       if (!objectEditable) {
                         if (!isScale) return;
                         if (pointerCount == 2) {
-                          const double step = 0.00015;
+                          const double step = 0.00012;
 
                           if (details.verticalScale > 1) {
                             mScaleY = (mScaleY + step).clamp(0.9, 1.0);
